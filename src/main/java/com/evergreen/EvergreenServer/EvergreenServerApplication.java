c48baced414a149b09b55f322da6b25dfada855f
ratroot92 <zcomisb.007@gmail.com>
2026-01-19 19:20:08 +0500

    it will add implementations for java util streams to unerstand them better

--- src/main/java/com/evergreen/EvergreenServer/EvergreenServerApplication.java
+++ src/main/java/com/evergreen/EvergreenServer/EvergreenServerApplication.java
@@ -3,6 +3,9 @@ package com.evergreen.EvergreenServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import com.evergreen.EvergreenServer.collections.SampleFilter;
+import com.evergreen.EvergreenServer.collections.SampleFlatMap;
+import com.evergreen.EvergreenServer.collections.SampleMap;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
@@ -10,6 +13,25 @@ public class EvergreenServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EvergreenServerApplication.class, args);
+
+        // Filter examples
+        SampleFilter sampleFilter = new SampleFilter();
+        sampleFilter.filterPassedStudent();
+        System.out.println("================================");
+        sampleFilter.filterPassedStudentAndForEach();
+
+        // Map examples
+        SampleMap sampleMap = new SampleMap();
+        sampleMap.convertNamesFirstLetterToUpperCaseAndPrint();
+        sampleMap.convertStudentNamesToCapitalCaseAndPrint();
+        sampleMap.sortStudentByMarks();
+
+
+        // FlatMap
+        SampleFlatMap sampleFlatMap = new SampleFlatMap();
+        sampleFlatMap.show();
+
+
     }
 
 
---
+++ src/main/java/com/evergreen/EvergreenServer/collections/example_classes/ExampleStudent.java
@@ -0,0 +1,17 @@
+package com.evergreen.EvergreenServer.collections.example_classes;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ExampleStudent {
+
+    private int id;
+    private String name;
+    private int marks;
+
+
+}
